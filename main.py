e4634039086cdcf6f855a7bba725c7b97df53296
csuftitan <csuftitan@macbook-air.lan>
2023-01-17 22:43:51 -0800

    removed some items

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ class CollectData(GridLayout):  # creating the structure
         self.add_widget(Label(text='What is your name: '))
         self.name = TextInput()  # type name into text box
         self.add_widget(self.name)
-        self.name.size_hint = (0.5, 0.5)
         self.name.halign = 'center'
         self.name.valign = 'center'
 
@@ -28,55 +27,43 @@ class CollectData(GridLayout):  # creating the structure
         self.add_widget(Label(text='Enter one side of Square: '))
         self.square = MyIntegerInput()  # type name into text box
         self.add_widget(self.square)
-        self.square.size = (0.5, 0.5)
-        self.square.halign = 'center'
-        self.square.valign = 'center'
-        self.square.width = 300
+        self.name.halign = 'center'
+        self.name.valign = 'center'
 
         """Enter one side of a Circle below """
         self.add_widget(Label(text='Enter Radius of Circle'))
         self.circle = MyIntegerInput()  # type name into text box
         self.add_widget(self.circle)
-        self.circle.size = (0.5, 0.5)
-        self.circle.halign = 'center'
-        self.circle.valign = 'center'
-        self.circle.width = 300
+        self.name.halign = 'center'
+        self.name.valign = 'center'
 
         """Enter Height of a Triangle below """
         self.add_widget(Label(text='Enter height of Triangle'))
         self.triangleH = MyIntegerInput()  # type name into text box
         self.add_widget(self.triangleH)
-        self.triangleH.size = (0.5, 0.5)
-        self.triangleH.halign = 'center'
-        self.triangleH.valign = 'center'
-        self.triangleH.width = 300
+        self.name.halign = 'center'
+        self.name.valign = 'center'
 
         """Enter Base below """
         self.add_widget(Label(text='Enter Base of Triangle'))
         self.triangleB = MyIntegerInput()  # type name into text box
         self.add_widget(self.triangleB)
-        self.triangleB.size = (0.5, 0.5)
-        self.triangleB.halign = 'center'
-        self.triangleB.valign = 'center'
-        self.triangleB.width = 300
+        self.name.halign = 'center'
+        self.name.valign = 'center'
 
         """Enter the name of Ellipse axis a and axis b gender below """
         self.add_widget(Label(text='Enter axis 1 of Elipse'))
         self.axis1 = MyIntegerInput()  # type name into text box
         self.add_widget(self.axis1)
-        self.axis1.size = (0.5, 0.5)
-        self.axis1.halign = 'center'
-        self.axis1.valign = 'center'
-        self.axis1.width = 300
+        self.name.halign = 'center'
+        self.name.valign = 'center'
 
         """Enter the name of Ellipse axis a and axis b gender below """
         self.add_widget(Label(text='Enter axis 2 of Ellipse'))
         self.axis2 = MyIntegerInput()  # type name into text box
         self.add_widget(self.axis2)
-        self.axis2.size = (0.5, 0.5)
-        self.axis2.halign = 'center'
-        self.axis2.valign = 'center'
-        self.axis2.width = 300
+        self.name.halign = 'center'
+        self.name.valign = 'center'
 
         """button design next"""
         self.press = Button(text="Click to find Area")
@@ -84,8 +71,6 @@ class CollectData(GridLayout):  # creating the structure
         self.add_widget(self.press)
         self.output = Label(text='')  # Create an empty label
         self.add_widget(self.output)  # Add the label to the
-        self.output.halign = 'center'
-        self.output.valign = 'center'
         self.output.color = (1, 0, 0, 1)
 
     def click_me(self, instance):
